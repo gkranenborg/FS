a92413d86d790ccbe2f6fe1aa2b2e02bcc117538
gkranenborg <gerben.kranenborg@us.fujitsu.com>
2017-12-05 16:03:55 -0500

    Update for production

--- Production/jcrinstantbck.bat
+++ Production/jcrinstantbck.bat
@@ -1,6 +1,6 @@
 @echo off
 set SOURCEDIR=E:\FeinSuch\jcr\jackrabbit
-set TARGETDIR=E:\jcrinstant
+set TARGETDIR=E:\jcrbackup
 (for /f "tokens=4,* delims=\" %%a in (copyfromprod.txt) do echo %%b) > output.txt
 TIMEOUT /T 10 /NOBREAK
 (for /f %%A in (output.txt) do echo f | xcopy /f /y %SOURCEDIR%\%%A %TARGETDIR%\%%A)
